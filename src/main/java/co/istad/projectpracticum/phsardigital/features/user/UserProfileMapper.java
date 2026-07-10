c78ceaddac343269a63c0384304f05b4e83f4fee
unknown <mrrsaki0403@gmail.com>
2026-07-11 01:04:11 +0700

    User Update + Me

--- src/main/java/co/istad/projectpracticum/phsardigital/features/user/UserProfileMapper.java
+++ src/main/java/co/istad/projectpracticum/phsardigital/features/user/UserProfileMapper.java
@@ -1,6 +1,7 @@
 package co.istad.projectpracticum.phsardigital.features.user;
 
 import co.istad.projectpracticum.phsardigital.features.user.dto.UserProfileResponse;
+import org.keycloak.representations.idm.UserRepresentation;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -8,8 +9,12 @@ import org.mapstruct.Mapping;
 public abstract class UserProfileMapper {
     public UserProfileResponse toResponse(UserProfile p) {
         return new UserProfileResponse(
-                p.getId(), p.getEmail(), p.getFullName(), p.getPhone(),
-                p.getAvatarUrl(), p.getStatus() != null ? p.getStatus().name() : null,
+                p.getId(),
+                p.getEmail(),
+                p.getFullName(),
+                p.getPhone(),
+                p.getAvatarUrl(),
+                p.getStatus() != null ? p.getStatus().name() : null,
                 p.getDateOfBirth()
         );
     }
